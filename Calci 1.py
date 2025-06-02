c33cf41bf00d23bcd6fbce8924dc771ba7fe9161
SachinB02 <130825294+SachinB02@users.noreply.github.com>
2025-06-02 11:57:43 +0530

    Add files via upload

---
+++ Calci 1.py
@@ -0,0 +1,11 @@
+a = 100
+b = 20
+
+print("the value of", a, "+", b, "is:", a + b )
+print("the value of", a, "-", b, "is:", a - b )
+print("the value of", a, "*", b, "is:", a * b )
+print("the value of", a, "/", b, "is:", a / b )
+print("the value of", a, "%", b, "is:", a % b )
+print("the value of", a, "//", b, "is:", a // b )
+print("the value of", a, "**", b, "is:", a ** b )
+
